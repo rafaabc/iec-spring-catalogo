7b5c4d774e9bcaf06feb7e7691dfd5458ad96403
rafaabc <faelsabc21@gmail.com>
2022-03-20 13:34:25 -0300

    Cria bassociação categoria musica @ManyToOne

--- src/main/java/pro/gsilva/catalogo/model/Musica.java
+++ src/main/java/pro/gsilva/catalogo/model/Musica.java
@@ -3,12 +3,7 @@ package pro.gsilva.catalogo.model;
 
 import java.time.LocalDate;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Lob;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -37,5 +32,8 @@ public class Musica {
     @Lob
     private String letra;
 
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private Categoria categoria;
     
 }
